56bdc04dea86dfd99f968a1104ea46f56ab1b4b3
Oliv-14 <sulistianaolivia@gmail.com>
2025-09-30 07:30:45 +0700

    first commuit

---
+++ Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_first_app.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
